bac1fc18527a339a03c6746d2ecab59f12f9534a
Kenneth Kwan <khkwan0@gmail.com>
2022-04-01 03:07:12 +0000

    fixes

--- setup.sh
+++ setup.sh
@@ -7,6 +7,14 @@ sudo apt-get install -y powerline fonts-powerline
 sudo apt-get install zsh-syntax-highlighting
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-cp ./vimrc ~/.vimrc
-cp ./tmuxconf ~/.tmux.conf
-cp ./zshrc ~/.zshrc
+cp ./vimrc $HOME/.vimrc
+cp ./tmuxconf $HOME/.tmux.conf
+cp ./zshrc $HOME/.zshrc
+
+#docker
+sudo apt-get -y install ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
